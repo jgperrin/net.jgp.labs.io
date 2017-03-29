e74f8a9a7acb59575210ffd9d513e803fe656616
Jean Georges Perrin <jgp@jgp.net>
2017-03-29 18:10:17 -0400

    read file with IO and NIO

--- src/main/java/net/jgp/labs/io/file/FileIO.java
+++ src/main/java/net/jgp/labs/io/file/FileIO.java
@@ -1,5 +1,23 @@
 package net.jgp.labs.io.file;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class FileIO {
+
+	public static StringBuffer fileToStringBuffer(String filename) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		File f = new File(filename);
+		FileInputStream fis = new FileInputStream(f);
+		BufferedInputStream bif = new BufferedInputStream(fis);
+		byte b[] = new byte[4096];
+		while (bif.read(b) != -1) {
+			sb.append(new String(b));
+		}
+		bif.close();
+		return sb;
+	}
 
 }
